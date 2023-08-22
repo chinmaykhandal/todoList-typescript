e48006c44cab49b9eca5daeca790cdfb29419650
chinmaykhandal <khandalchinmay@gmail.com>
2023-08-22 20:44:15 +0530

    seperate sections for tasks

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,22 +1,43 @@
-import React from 'react'
+import React from "react";
 import "./styles.css";
-import { Todo } from '../model';
-import SingleTodo from './SingleTodo';
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
 
-interface Props{
-    todos: Todo[],
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
-    
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
 
-const TodoList:React.FC<Props> = ({todos, setTodos}) => {
+const TodoList: React.FC<Props> = ({ todos, setTodos }) => {
   return (
-    <div>
-      {todos.map((todo) => (
-        <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos}/>
-      ))}
-    </div>
-  )
-}
+    <div className="container">
+      <div className="todos">
+        <span className="todos__heading">Active Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
 
-export default TodoList
+      <div className="todos remove">
+      <div className="todos">
+        <span className="todos__heading">Completed Tasks</span>
+        {todos.map((todo) => (
+          <SingleTodo
+            todo={todo}
+            key={todo.id}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        ))}
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;
